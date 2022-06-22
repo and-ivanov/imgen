c94884c0f50eafbbcac9cc6bdd8df5f90c4967c4
Andrei Ivanov <9387186+am-ivanov@users.noreply.github.com>
2022-06-22 16:33:33 +0200

    Tweak a bit

--- imgen.py
+++ imgen.py
@@ -2,84 +2,126 @@ from skimage.io import imread, imsave
 from skimage.viewer import ImageViewer
 import numpy as np
 import random
+import inspect
+import scipy
+import matplotlib as mpl
 
-from numpy.polynomial import Polynomial
-
-p = Polynomial.fromroots([(0.2, 0.2), (0.7, 15)])
-
-res = p(np.linspace(0,1,num=10))
-
-
-print(res)
-
-exit()
-
-H = 600
-W = 400
+H = 1080
+W = 1920
 C = 3
+
 
 def scale_range(x, a, b):
     if x.max() - x.min() < 0.001:
         return np.ones_like(x) * (a + b) * 0.5
-    return np.interp(x, (x.min(), x.max()), (a, b))
+    # use quantiles to ignore outliers
+    x_min = np.quantile(x, 0.05)
+    x_max = np.quantile(x, 0.95)
+    x = np.clip(x, x_min, x_max)
+    return np.interp(x, (x_min, x_max), (a, b))
 
-image = np.zeros((H, W, C))
+image = np.zeros((H, W))
 
 def get_rand():
-    return np.ones_like(image) * np.random.rand(3)
+    return np.ones_like(image) * random.random()
 def get_x():
-    return np.ones_like(image) * np.linspace(-1.0, 1.0, W).reshape(1, W, 1)
+    return np.ones_like(image) * np.linspace(0.1, 0.9, W).reshape(1, W)
 def get_y():
-    return np.ones_like(image) * np.linspace(-1.0, 1.0, H).reshape(H, 1, 1)
+    return np.ones_like(image) * np.linspace(0.1, 0.9, H).reshape(H, 1)
 def div_ab(a, b):
     return np.divide(a, np.maximum(np.abs(b), 0.001))
-def div_ba(a, b):
-    return np.divide(b, np.maximum(np.abs(a), 0.001))
 def safe_log(x):
-    return np.log(np.maximum(np.abs(x), 0.001))
+    return scale_range(np.log(scale_range(x, 0.5, 2.0)), 0, 1)
 def safe_arcsin(a):
     return np.arcsin(scale_range(a, -1.0, 1.0))
 def safe_arccos(a):
     return np.arccos(scale_range(a, -1.0, 1.0))
 def safe_power(a, b):
-    return np.power(scale_range(a, 0.001, 1.0), scale_range(b, 0.001, 1.0))
+    res = np.power(scale_range(a, 0.5, 2.0), scale_range(b, 0.5, 2.0))
+    return scale_range(res, 0, 1)
 def safe_sin(a):
     return np.sin(scale_range(a, -5, 5))
 def safe_cos(a):
     return np.cos(scale_range(a, -5, 5))
+def sincos(a):
+    if np.random.randint(0, 2) == 1:
+        r = safe_sin(a)
+    else:
+        r = safe_cos(a)
+    return scale_range(a, 0, 1)
 def polynomial(x, y):
-    pass
+    r = np.ones_like(image)
+    p = random.randrange(-2, 3)
+    for root in range(random.randrange(3, 7)):
+        x0, y0 = random.uniform(0.1, 0.9), random.uniform(0.1, 0.9)
+        if p == 0:
+            r *= (np.abs(x - x0) + np.abs(y - y0))
+        else:
+            r *= (np.abs(x - x0)**p + np.abs(y - y0)**p)**(1/p)
+    return scale_range(r, 0, 1)
+def poly_root():
+    return polynomial(get_x(), get_y())
+def convolve(x):
+    conv_size = 5
+    template = np.random.random_sample(size=(conv_size, conv_size))
+    convolved = scipy.signal.convolve(x, template, mode='valid')
+    padded = np.pad(convolved, pad_width=conv_size // 2, mode='edge')
+    return scale_range(padded, 0, 1)
+def mix(a, b):
+    return (a + b) / 2
+def mirror(x):
+    cutoff = random.random()
+    return (x < cutoff) * (cutoff - x) + (x > cutoff) * x 
+def gradient(a):
+    dx, dy = np.gradient(a)
+    if np.random.randint(0, 2) == 1:
+        grad = dx
+    else:
+        grad = dy
+    return scale_range(grad, 0, 1)
+def stack(c, a, b):
+    cutoff = random.random()
+    return (c < cutoff) * a + (c > cutoff) * b
 
-functions = [(0, get_rand), # -> [0,1]
-             (0, get_x), # -> [0,1]
-             (0, get_y), # -> [0,1]
-             (1, safe_sin), # R -> [-1,1]
-             (1, safe_cos), # R -> [-1,1]
-             (1, safe_arcsin), # [-1,1] -> -pi/2, pi/2
-             (1, safe_arccos),
-            #  (1, safe_log), # R+ -> R
-             (2, np.add), # [b1,e1], [b2,e2] -> [min(b1,b2), max(e1,e2)]
-             (2, np.subtract), # [b1,e1], [b2,e2] -> [b1-e2, e1-b2]
-             (2, np.multiply), # [b1,e1], [b2,e2] -> [min(_*_), max(_*_)]
-            #  (2, div_ab), # [a1,a2], [b1, b2] -> [min(_/_), max(_/_)]
-            #  (2, div_ba),
-             (2, safe_power), # [a1,a2], [b1, b2] -> [a1^b1, a2^b2]
-             ]
-depthMin = 7
-depthMax = 20
+functions = [
+    get_rand,
+    poly_root,
+    sincos,
+    polynomial,
+    convolve,
+    mirror,
+    safe_power,
+    mix,
+    gradient,
+    safe_log,
+    stack,
+]
+
+def num_args(func):
+    return len(inspect.signature(func).parameters)
+
+depthMin = 4
+depthMax = 10
 
 def buildImg(depth = 0):
     funcs = [f for f in functions if
-                (f[0] > 0 and depth < depthMax) or
-                (f[0] == 0 and depth >= depthMin)]
-    nArgs, func = random.choice(funcs)
-    args = [buildImg(depth + 1) for n in range(nArgs)]
+                (num_args(f) > 0 and depth < depthMax) or
+                (num_args(f) == 0 and depth >= depthMin)]
+    func = random.choice(funcs)
+    args = [buildImg(depth + 1) for n in range(num_args(func))]
     return func(*args)
 
 with np.errstate(**{'divide': 'raise', 'over': 'raise', 'under': 'raise', 'invalid': 'raise'}):
     image = buildImg()
 
-image = scale_range(image, 0, 255).astype(np.uint8)
+available_colormaps = list(mpl.colormaps.keys())
+colormap_name = random.choice(available_colormaps)
+#colormap_name = 'hsv'
+print(f'colormap: {colormap_name}')
+colormap = mpl.cm.get_cmap(colormap_name)
+
+image = colormap(scale_range(image, 0, 1))[:,:,0:3]
+image = (image * 255).astype(np.uint8)
 imsave('test.png', image)
 #image = imread('test.png')
 # view = ImageViewer(image)
